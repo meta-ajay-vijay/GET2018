89ccde93fb2d2d612bb2efc6e3875eee22002b7a
meta-ajay-vijay <41181710+meta-ajay-vijay@users.noreply.github.com>
2018-08-09 13:44:18 +0530

    multivariate polynomial

---
+++ multivariatePolynomial/Variable.java
@@ -0,0 +1,25 @@
+package multivariatePolynomial;
+
+public class Variable {
+	private char variableSymbol;
+	private int power;
+
+	public Variable(char variableSymbol, int power) {
+		this.variableSymbol = variableSymbol;
+		this.power = power;
+	}
+
+	/**
+	 * @return the variableSymbol
+	 */
+	public char getVariableSymbol() {
+		return variableSymbol;
+	}
+	/**
+	 * @return the power
+	 */
+	public int getPower() {
+		return power;
+	}
+
+}
