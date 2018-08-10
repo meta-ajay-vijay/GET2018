0d3988c0974ad73919ac38a1d92d826b49c108ad
meta-ajay-vijay <41181710+meta-ajay-vijay@users.noreply.github.com>
2018-08-10 17:43:17 +0530

    updated Dictionary.java

--- Dictionary/DictionaryClass.java
+++ Dictionary/DictionaryClass.java
@@ -16,13 +16,13 @@ import org.apache.commons.collections4.map.HashedMap;
  */
 public class DictionaryClass implements Dictionary {
 
-	Map<String, BST> dictionaryMap = new HashedMap<String, BST>();
-	List<String> wordsList = new ArrayList<String>();
+	// Map<String, BST> dictionaryMap = new HashedMap<String, BST>();
+	static List<String> wordsList = new ArrayList<String>();
 	BST rootOfDictionary;
 
 	/**
-	 * method checks that word in BST object exists in dictionary or not, if exist
-	 * then returns false and appropriate message, otherwise returns true
+	 * method checks that word in BST object exists in dictionary or not, if
+	 * exist then returns false and appropriate message, otherwise returns true
 	 * 
 	 * @param objectToAddInDictionary
 	 * 
@@ -31,7 +31,7 @@ public class DictionaryClass implements Dictionary {
 	public booleanAndString checkWordExistOrNot(BST objectToAddInDictionary) {
 		booleanAndString resultOfInsertion = new booleanAndString();
 
-		if (dictionaryMap.containsKey(objectToAddInDictionary.getKeyString())) {
+		if (wordsList.contains(objectToAddInDictionary.getKeyString().toUpperCase())) {
 			resultOfInsertion.setFlag(false);
 			resultOfInsertion.setMessage("Word already exist in dictionary");
 		} else {
@@ -49,30 +49,54 @@ public class DictionaryClass implements Dictionary {
 	 */
 	@Override
 	public void addInDictionary(BST objectToAddInDictionary) {
-		// If first entry in dictionary then make it root else add BST by recusion
-		if (wordsList.size() == 0) {
+		// If first entry in dictionary then make it root else add BST by
+		// recusion
+		if (rootOfDictionary == null) {
 			rootOfDictionary = objectToAddInDictionary;
-			dictionaryMap.put(objectToAddInDictionary.getKeyString(), objectToAddInDictionary);
+			rootOfDictionary.setParent(null);
+			System.out.println("Root");
+			wordsList.add(objectToAddInDictionary.getKeyString().toUpperCase());
+			// dictionaryMap.put(objectToAddInDictionary.getKeyString(),
+			// objectToAddInDictionary);
 		} else {
 			addInDictionaryByRecursion(objectToAddInDictionary, rootOfDictionary);
+
 		}
 	}
 
+	/**
+	 * 
+	 * @param objectToAddInDictionary
+	 *            Word that we want to add in dictionary
+	 * @param bstValue
+	 */
 	private void addInDictionaryByRecursion(BST objectToAddInDictionary, BST bstValue) {
 
-		if (bstValue.getKeyString().compareToIgnoreCase(objectToAddInDictionary.getKeyString()) < 0) {
+		if (objectToAddInDictionary.getKeyString().compareToIgnoreCase(bstValue.getKeyString()) > 0) {
 			if (bstValue.getRightBST() == null) {
+				objectToAddInDictionary.setParent(bstValue);
 				bstValue.setRightBST(objectToAddInDictionary);
-				dictionaryMap.put(bstValue.getKeyString(), bstValue);
+				wordsList.add(objectToAddInDictionary.getKeyString().toUpperCase());
+				// dictionaryMap.put(bstValue.getKeyString(), bstValue);
+				System.out.println("Added to Right");
+				return;
 			} else {
+				System.out.println("Going to the Right...");
 				addInDictionaryByRecursion(objectToAddInDictionary, bstValue.getRightBST());
+				return;
 			}
-		} else if (bstValue.getKeyString().compareToIgnoreCase(objectToAddInDictionary.getKeyString()) > 0) {
+		} else if (objectToAddInDictionary.getKeyString().compareToIgnoreCase(bstValue.getKeyString()) < 0) {
 			if (bstValue.getLeftBST() == null) {
+				objectToAddInDictionary.setParent(bstValue);
 				bstValue.setLeftBST(objectToAddInDictionary);
-				dictionaryMap.put(bstValue.getKeyString(), bstValue);
+				// dictionaryMap.put(bstValue.getKeyString(), bstValue);
+				wordsList.add(objectToAddInDictionary.getKeyString().toUpperCase());
+				System.out.println("Added to Left");
+				return;
 			} else {
+				System.out.println("Going to the Left...");
 				addInDictionaryByRecursion(objectToAddInDictionary, bstValue.getLeftBST());
+				return;
 			}
 		}
 
@@ -85,7 +109,6 @@ public class DictionaryClass implements Dictionary {
 	 */
 	@Override
 	public boolean deleteFromDictionary(String wordToDelete) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
@@ -97,13 +120,47 @@ public class DictionaryClass implements Dictionary {
 	@Override
 	public booleanAndString getValueFromDictionary(String keyInDictionary) {
 		booleanAndString resultToReturn = new booleanAndString();
-		if(dictionaryMap.containsKey(keyInDictionary)) {
-			resultToReturn.setFlag(true);
-			resultToReturn.setMessage(dictionaryMap.get(keyInDictionary).getValueString());
+		if (wordsList.size() > 1) {
+			resultToReturn = getValueByRecursion(keyInDictionary, rootOfDictionary);
 		} else {
-			resultToReturn.setFlag(false);
-			resultToReturn.setMessage("No such word in dictionary");
+			if (rootOfDictionary == null) {
+				resultToReturn.setFlag(false);
+				resultToReturn.setMessage("Nothing in dictionary");
+			} else {
+				if (rootOfDictionary.getKeyString().equals(keyInDictionary)) {
+					resultToReturn.setFlag(true);
+					resultToReturn.setMessage("Found");
+				} else {
+					resultToReturn.setFlag(false);
+					resultToReturn.setMessage("NotFound");
+				}
+			}
 		}
+
+		return resultToReturn;
+	}
+
+	public booleanAndString getValueByRecursion(String keyInDictionary, BST parentOfKeyBeingSearched) {
+		booleanAndString resultToReturn = new booleanAndString();
+
+		if (parentOfKeyBeingSearched.getLeftBST() == null && parentOfKeyBeingSearched.getRightBST() == null) {
+			if (!parentOfKeyBeingSearched.getKeyString().equals(keyInDictionary)) {
+				resultToReturn.setFlag(false);
+				resultToReturn.setMessage("Word not found");
+			}
+		}
+		if (parentOfKeyBeingSearched.getKeyString().compareToIgnoreCase(keyInDictionary) == 0) {
+			resultToReturn.setFlag(true);
+			resultToReturn.setMessage(parentOfKeyBeingSearched.getValueString());
+
+		} else if (parentOfKeyBeingSearched.getKeyString().compareToIgnoreCase(keyInDictionary) < 0) {
+			resultToReturn = getValueByRecursion(keyInDictionary, parentOfKeyBeingSearched.getRightBST());
+
+		} else if (parentOfKeyBeingSearched.getKeyString().compareToIgnoreCase(keyInDictionary) > 0) {
+			resultToReturn = getValueByRecursion(keyInDictionary, parentOfKeyBeingSearched.getLeftBST());
+
+		}
+
 		return resultToReturn;
 	}
 
@@ -113,9 +170,16 @@ public class DictionaryClass implements Dictionary {
 	 * @see Dictionary.Dictionary#sortedDictionary()
 	 */
 	@Override
-	public List<Dictionary> sortedDictionary() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<BST> sortedDictionary(BST parentNode) {
+		List<BST> listToAddInSortedOrder = new ArrayList<BST>();
+		if(parentNode.getLeftBST() != null){
+			listToAddInSortedOrder.addAll(sortedDictionary(parentNode.getLeftBST()));
+		}
+		listToAddInSortedOrder.add(parentNode);
+		if(parentNode.getRightBST() != null){
+			listToAddInSortedOrder.addAll(sortedDictionary(parentNode.getRightBST()));
+		}
+		return listToAddInSortedOrder;
 	}
 
 	/*
@@ -126,7 +190,6 @@ public class DictionaryClass implements Dictionary {
 	 */
 	@Override
 	public List<Dictionary> sortedDictionary(String fromKey, String toKey) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
