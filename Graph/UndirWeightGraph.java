7a498f5cf2d964865b6c6a486656c4765b56548b
meta-ajay-vijay <41181710+meta-ajay-vijay@users.noreply.github.com>
2018-08-13 00:20:44 +0530

    Graph Assignment

---
+++ Graph/UndirWeightGraph.java
@@ -0,0 +1,14 @@
+package Graph;
+
+import java.util.List;
+
+public interface UndirWeightGraph {
+
+	public boolean isConnected();
+	
+	public int[] reachable(int vertex);
+	
+	public int[][] getMinimumSpanningTree();
+	
+	public int shortestPath(int source, int destination);
+}
