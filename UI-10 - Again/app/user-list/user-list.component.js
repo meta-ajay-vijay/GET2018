8ff344e74bc0ee00f112518ecf92c3796382f1e3
meta-ajay-vijay <41181710+meta-ajay-vijay@users.noreply.github.com>
2018-10-23 15:28:23 +0530

    UI-10 again

---
+++ UI-10 - Again/app/user-list/user-list.component.js
@@ -0,0 +1,70 @@
+'use strict';
+
+angular.module('userList').
+    component('userList',{
+        templateUrl: 'user-list/user-list.template.html',
+        controller: function UserListController($scope,$http){
+            
+            $scope.userData;
+
+            var self= this;
+
+            $http.get('http://localhost:3000/users').then(function(response){
+                self.users = response.data;
+            });
+
+            /*$scope.postData=function(){
+                $http({
+                    method:'POST',
+                    url:'http://localhost:3000/users/',
+                    data: $scope.user,
+                    dataType:'json'
+                });
+
+                window.location.reload();
+            };*/
+            $scope.postData=function(){
+             if($scope.user.id == null){
+                    console.log("Post ho rha h");
+                    $http({
+                      method: 'POST',
+                      url:'http://localhost:3000/users/',
+                      data: $scope.user,
+                      dataType:'json'
+                    });
+                 }else{
+                    console.log("Put ho rha h");
+                    $http.put('http://localhost:3000/users/' + $scope.user.id,$scope.user);
+                 }
+                 console.log("Jo hona tha ho gya");
+                $scope.resetUser();
+                window.location.reload();
+              }
+
+            $scope.editUser = function(id){
+                 console.log(id);
+                 console.log("Id get ho rha h");
+                 $http({
+                      method: 'GET',
+                      url: 'http://localhost:3000/users/' + id
+                     }).then(function (response){
+                    $scope.user = response.data;
+                    });
+                    console.log("Jo hona tha ho gya");
+                }
+
+             $scope.resetUser = function(){
+                  let user = {
+                    "id" : null,
+                    "name": "",
+                    "phone": "",
+                    "location": ""
+                  };
+                  $scope.user = user;
+                }
+
+          /*  $scope.show=function(userData){
+                self.userData = userData;
+            }*/
+        }
+    });
